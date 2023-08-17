7a8db6ccff7298eff8e639c06d5ae9caec46ce49
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-08-17 02:32:38 -0700

    add map to view and set config

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body : FlutterMap(
+        options: MapOptions(
+          center: const LatLng(35.715298, 51.404343),
+          zoom: 16,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+            backgroundColor: Colors.blue,
+          )
+        ],
+      )
+    );
   }
 }
